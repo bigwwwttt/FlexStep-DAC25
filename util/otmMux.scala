404a8ccc68bbf287505f5a3f05e1a06ea4c82f46
bigwwwttt <151817548+bigwwwttt@users.noreply.github.com>
2024-01-20 10:51:35 +0800

    Update otmMux.scala

--- util/otmMux.scala
+++ util/otmMux.scala
@@ -2,25 +2,32 @@ package freechips.rocketchip.util
 
 import chisel3._
 import chisel3.util._
+import freechips.rocketchip.util.GlobalParams
 
 class otmMux(num: Int) extends Module{
     val io = IO(new Bundle{
-        val in = Flipped(Decoupled(UInt(32.W)))
-        val out = Vec(num, Decoupled(UInt(32.W)))
+        val in = Flipped(Decoupled(GlobalParams.Data_type))
+        val out = Vec(num, Decoupled(GlobalParams.Data_type))
         val sels = Input(Vec(num, Bool()))
     })
     val readyVec = io.out.zip(io.sels).map { case (out, sel) =>
-    out.ready && sel
+    (!out.ready) && sel
     }
-    io.in.ready := PriorityMux(io.sels, readyVec)
+    io.in.ready := !(readyVec.reduce(_ || _))
     for(i <- 0 until num){
         when(io.sels(i)){
-            io.in <> io.out(i)
+            when(!(readyVec.reduce(_ || _))){
+               io.in.bits <> io.out(i).bits
+               io.in.valid <> io.out(i).valid
+            }.otherwise{
+               io.out(i).valid := false.B
+               io.out(i).bits := 0.U
+            }        
         }.otherwise{
             io.out(i).valid := false.B
             io.out(i).bits := 0.U
         }
     }
-    
+        
 }
 
