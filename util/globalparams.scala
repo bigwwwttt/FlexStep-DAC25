7e576d51ae1b8abfd462e7e5d5ed43ec4d283856
bigwwwttt <151817548+bigwwwttt@users.noreply.github.com>
2024-01-20 10:29:31 +0800

    Update globalparams.scala

--- util/globalparams.scala
+++ util/globalparams.scala
@@ -4,12 +4,17 @@ import chisel3._
 import chisel3.util._
 
 object GlobalParams {
-  val Num_cores = 8;
-  val Num_Slavecores = Num_cores - 1
-  var Master_core = 0
+  val Num_cores = 6;
+  val Num_Mastercores = 2
+  val Num_Slavecores = Num_cores - Num_Mastercores
   val List_hartid = (0 until Num_cores by 1).toList
-  var List_Slaveid = List_hartid.filter(_ != Master_core)
   val Data_width = 32
   val Data_type = UInt(Data_width.W)
   val depth = 32
+}
+
+object GlobalVariables {
+  var List_MasterId = List(0, 1)
+  val List_SlaveId = GlobalParams.List_hartid.filterNot(List_MasterId.contains)
+  require(List_MasterId.length == GlobalParams.Num_Mastercores)
 }
