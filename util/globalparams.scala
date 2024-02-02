45124e5641afa16e8f42c7c7cf29f1f32bc89149
bigwwwttt <151817548+bigwwwttt@users.noreply.github.com>
2024-02-02 17:43:42 +0800

    Update globalparams.scala

--- util/globalparams.scala
+++ util/globalparams.scala
@@ -1,20 +1,25 @@
-package freechips.rocketchip.tile
+package freechips.rocketchip.util
 
 import chisel3._
-import chisel3.util._
 
 object GlobalParams {
-  val Num_cores = 6;
-  val Num_Mastercores = 2
-  val Num_Slavecores = Num_cores - Num_Mastercores
+  val Num_cores = 8;
+  val Num_Groupcores = Num_cores / 2
   val List_hartid = (0 until Num_cores by 1).toList
+  val List_hartid1 = (0 until Num_Groupcores by 1).toList
+  val List_hartid2 = (Num_Groupcores until Num_cores by 1).toList
+
+
+  var List_MasterId1 = List(0, 1, 2)
+  var List_MasterId2 = List(4, 5)
+  var Num_Mastercores1 = List_MasterId1.length
+  var Num_Slavecores1 = Num_Groupcores - Num_Mastercores1
+  var Num_Mastercores2 = List_MasterId2.length
+  var Num_Slavecores2 = Num_Groupcores - Num_Mastercores2
+  var List_SlaveId1 = List_hartid1.filterNot(List_MasterId1.contains)
+  var List_SlaveId2 = List_hartid2.filterNot(List_MasterId2.contains)
+  
   val Data_width = 32
   val Data_type = UInt(Data_width.W)
   val depth = 32
-}
-
-object GlobalVariables {
-  var List_MasterId = List(0, 1)
-  val List_SlaveId = GlobalParams.List_hartid.filterNot(List_MasterId.contains)
-  require(List_MasterId.length == GlobalParams.Num_Mastercores)
 }
