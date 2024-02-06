35db7722689312e5e560d844558c769e7373290f
bigwwwttt <151817548+bigwwwttt@users.noreply.github.com>
2024-02-06 16:28:51 +0800

    Update otmMux.scala

--- util/otmMux.scala
+++ util/otmMux.scala
@@ -13,16 +13,22 @@ class otmMux(num: Int) extends Module{
     val readyVec = io.out.zip(io.sels).map { case (out, sel) =>
     (!out.ready) && sel
     }
-    io.in.ready := !(readyVec.reduce(_ || _))
+    val NotallFfalse = io.sels.reduce(_ || _)
+    when(NotallFfalse){
+        io.in.ready := !(readyVec.reduce(_ || _))
+    }.otherwise{
+        io.in.ready := false.B
+    }
+    
     for(i <- 0 until num){
         when(io.sels(i)){
-            when(!(readyVec.reduce(_ || _))){
-               io.in.bits <> io.out(i).bits
-               io.in.valid <> io.out(i).valid
+            when(io.in.ready){
+                io.in.bits <> io.out(i).bits
+                io.in.valid <> io.out(i).valid
             }.otherwise{
-               io.out(i).valid := false.B
-               io.out(i).bits := 0.U
-            }        
+                io.out(i).valid := false.B
+                io.out(i).bits := 0.U
+            }                 
         }.otherwise{
             io.out(i).valid := false.B
             io.out(i).bits := 0.U
@@ -30,4 +36,3 @@ class otmMux(num: Int) extends Module{
     }
         
 }
-
