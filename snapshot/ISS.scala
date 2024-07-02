88a29b72a8ac736bbcc6b5f2f3ab921b0cb84de1
bigwwwttt <151817548+bigwwwttt@users.noreply.github.com>
2024-07-02 10:28:28 +0800

    Update ISS.scala

--- snapshot/ISS.scala
+++ snapshot/ISS.scala
@@ -6,11 +6,11 @@ import chisel3.util._
 
 class ISS extends Module{
   val io = IO(new Bundle{
-    val copy_valid    =    Input(Bool())
-    val sent_valid    =    Input(Bool())
-
-    val copy_done     =    Output(Bool())
-    val sent_done     =    Output(Bool())
+    val copy_valid      =    Input(Bool())
+    val sent_valid      =    Input(Bool())
+  
+    val copy_done       =    Output(Bool())
+    val sent_done       =    Output(Bool())
     
     val intreg_input  =    Input(Vec(32, UInt(64.W)))
     val fpreg_input   =    Input(Vec(32, UInt(64.W)))
@@ -82,43 +82,43 @@ class ISS extends Module{
   val fp_coun = RegInit(0.U(5.W))
   val q_fp_coun = Wire(UInt())
   val fp_codo = Wire(Bool())
+
+  val rf_coun   = RegInit(0.U(5.W))
+  val q_rf_coun = Wire(UInt())
+  val rf_codo   = Wire(Bool())
   
-  val codo = Wire(Bool())
+  //val codo = Wire(Bool())
   
   q_int_coun := int_coun
   q_fp_coun := fp_coun
-  int_codo := (int_coun===31.U).asBool && (seq_signal === 0.U).asBool
-  fp_codo := (fp_coun===31.U).asBool && (seq_signal === 1.U).asBool
-  codo := (seq_signal === 2.U).asBool
+  q_rf_coun := rf_coun
+  int_codo := (int_coun===31.U).asBool && (seq_signal === 1.U).asBool
+  fp_codo := (fp_coun===31.U).asBool && (seq_signal === 2.U).asBool
+  rf_codo := (rf_coun === 31.U) && (seq_signal === 1.U) && io.sent_valid
+  //codo := (seq_signal === 0.U).asBool
 
   io.sign := seq_signal
   when(io.sent_valid){
-     seq_signal := Mux(int_codo || fp_codo || codo ,seq_signal+1.U , seq_signal)
+     seq_signal := Mux(rf_codo, 0.U, seq_signal)
      when(seq_signal === 0.U){
+        io.sent_output := Cat(data_sign, seq_signal, 0.U(203.W), intpcreg, fpcsrreg)
+        seq_signal := seq_signal + 1.U
+     }.elsewhen(seq_signal === 1.U){
+        rf_coun  := Mux(rf_codo, 0.U, rf_coun + 1.U)
         int_coun := Mux(int_codo , 0.U,int_coun+1.U)
-        io.sent_output := Cat(data_sign, seq_signal, int_coun, 0.U(182.W), intreg(q_int_coun))
-     }
-     .elsewhen(seq_signal === 1.U){
-        fp_coun := Mux(fp_codo , 0.U,fp_coun+1.U)
-        io.sent_output := Cat(data_sign, seq_signal, fp_coun, 0.U(182.W), fpreg(q_fp_coun))
-     }
-     .elsewhen(seq_signal === 2.U){
-        io.sent_output := Cat(data_sign, seq_signal, 0.U(203.W), intpcreg,fpcsrreg)
-     }
-     .otherwise{
+        io.sent_output := Cat(data_sign, seq_signal, rf_coun, 0.U(118.W), fpreg(q_rf_coun), intreg(q_rf_coun))
+     }.otherwise{
         seq_signal := 0.U
         io.sent_output := 0.U 
      }
-  }
-  .otherwise{
+  }.otherwise{
      io.sent_output := 0.U
-     seq_signal := 0.U
   }
   
-  when(codo){
+  when(rf_codo){
      io.sent_done := true.B
-  }
-  .otherwise{
+     seq_signal := 0.U
+  }.otherwise{
      io.sent_done := false.B
   }                       
 }
