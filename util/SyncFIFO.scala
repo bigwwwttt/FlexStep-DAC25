24dfc9a05d60c035ee3d269df24394d5011a83a8
bigwwwttt <151817548+bigwwwttt@users.noreply.github.com>
2024-07-02 10:26:46 +0800

    Update SyncFIFO.scala

--- util/SyncFIFO.scala
+++ util/SyncFIFO.scala
@@ -4,20 +4,29 @@ import chisel3._
 import chisel3.util._
 
 class SyncFIFO[T <: Data](ioType: T, depth: Int) extends Module {
-  val io = IO(new Bundle{
+  val io = IO(new Bundle {
     val in = Flipped(Decoupled(ioType))
     val out = Decoupled(ioType)
+    val almostfull = Output(Bool())
     val full = Output(Bool())
     val empty = Output(Bool())
     val widx = Output(UInt((log2Ceil(depth)).W))
     val ridx = Output(UInt((log2Ceil(depth)).W))
   })
-  require(depth>0 && isPow2(depth))
+  require(depth > 0 && isPow2(depth))
 
-  io.widx := Counter(io.in.valid && !io.full, depth)._1
-  io.ridx := Counter(io.out.ready && !io.empty, depth)._1
+  val queue = Module(new Queue(ioType, depth, false, false, true))
 
-  io.out <> Queue(io.in, depth)
-  io.full := !io.in.ready
-  io.empty := !io.out.valid
+  val almostfull = queue.io.count >= depth.U - 5.U
+  io.in <> queue.io.enq
+  queue.io.deq <> io.out
+
+  io.widx := Counter(io.in.valid && io.in.ready && !io.full, depth)._1
+  io.ridx := Counter(io.out.valid && io.out.ready && !io.empty, depth)._1
+
+  io.full := queue.io.count === depth.U
+  io.empty := queue.io.count === 0.U
+  io.almostfull := almostfull
 }
+
+
